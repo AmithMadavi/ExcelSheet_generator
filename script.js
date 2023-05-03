715d82662ec247aeeaf7f700f46eca093c852906
mukesh <suhassatheesh16@gmail.com>
2023-05-03 15:22:45 +0530

    added sweetalert

---
+++ script.js
@@ -0,0 +1,35 @@
+let table = document.getElementsByClassName("sheet-body")[0];
+let rows = document.getElementsByClassName("rows")[0];
+let columns = document.getElementsByClassName("columns")[0];
+let tableExists = false
+
+const generateTable = () => {
+    let rowsNumber = parseInt(rows.value);
+    let columnsNumber = parseInt(columns.value)
+    table.innerHTML = ""
+
+    for (let i = 0; i < rowsNumber; i++) {
+        var tableRow = ""
+        for (let j = 0; j < columnsNumber; j++) {
+            tableRow += `<td contenteditable></td>`
+        }
+        table.innerHTML += tableRow
+    }
+    if (rowsNumber > 0 && columnsNumber > 0) {
+        tableExists = true
+    } else {
+        swal("failed to generate");
+    }
+
+}
+
+const ExportToExcel = (type, fn, dl) => {
+    if (!tableExists) {
+        swal("error while exporting");
+        return
+    }
+    var elt = table
+    var wb = XLSX.utils.table_to_book(elt, { sheet: "sheet1" })
+    return dl ? XLSX.write(wb, { bookType: type, bookSST: true, type: 'base64' }) :
+        XLSX.writeFile(wb, fn || ('MyNewSheet.' + (type || 'xlsx')))
+}
